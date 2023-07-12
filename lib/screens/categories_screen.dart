d287ad562685d5bce9f85463ac14987dcee7a4d8
brendonrodri <rodriguesbrendon861@gmail.com>
2023-07-12 11:08:14 -0300

    feat: subindo repositório

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:foods/components/category_item.dart';
+import 'package:foods/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Vamos Cozinhar")),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
